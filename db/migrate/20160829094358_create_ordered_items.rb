32df3f46e23ecbce40f3d3ba60ff0c0042ea7dc8
Tun Amsyar <tun.amsyar@gmail.com>
2016-08-29 17:59:29 +0800

    added ordered items model

---
+++ db/migrate/20160829094358_create_ordered_items.rb
@@ -0,0 +1,10 @@
+class CreateOrderedItems < ActiveRecord::Migration[5.0]
+  def change
+    create_table :ordered_items do |t|
+      t.integer :item_id
+      t.integer :order_id
+
+      t.timestamps
+    end
+  end
+end
