e1adad9338f04cc682deff6863da26a609f570f7
Tun Amsyar <tun.amsyar@gmail.com>
2016-08-29 14:37:28 +0800

    fix merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get :about, to: 'static_pages#about'
+  root to:'landing#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
