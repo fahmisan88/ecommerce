8d14ca393cb384a14616ab60b933659932486781
fahmisan <ahmadfahmisan@gmail.com>
2016-08-30 11:22:28 +0800

    add controller order

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160829065850) do
+ActiveRecord::Schema.define(version: 20160829092413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.string   "item_name"
+    t.integer  "quantity"
+    t.float    "price"
+    t.float    "total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
