c614afbe18f39e4bb543f54c213d21265ed371b0
fahmisan <ahmadfahmisan@gmail.com>
2016-09-02 17:06:25 +0800

    done order summary and order controller

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,9 +1,21 @@
 class OrderController < ApplicationController
 
+
+
   def show
-    @items=[]
-
-
+    if cookies[:cart]
+      @cart = JSON.parse(cookies[:cart])
+    else
+      @cart = {}
+    end
+    
+    @items = []
+    @cart.each do |item_id,quantity|
+      item = Item.find_by(id: item_id)
+      item.define_singleton_method(:quantity) do
+        quantity
+      end
+      @items << item
+    end
   end
-
 end
