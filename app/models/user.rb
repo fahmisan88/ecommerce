3f3eded25c6294811a247195e9f9f16ab36bd923
Tun Amsyar <tun.amsyar@gmail.com>
2016-09-08 09:56:49 +0800

    users fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   validates :username, length:{minimum: 2}, presence: true
   enum role: [:user, :moderator, :admin]
 
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       user.provider = auth.provider
